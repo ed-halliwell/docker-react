35d3d5429767354cea37165234f40288ae193435
Ed Halliwell <edhalliwell@hotmail.com>
2022-04-27 13:52:08 +0100

    Updating text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React This is a change on the feature branch.
         </a>
       </header>
     </div>
